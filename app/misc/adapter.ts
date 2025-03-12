55401e5e3bffb013881d976d6668a53faed4dd1f
LukassF <szkulkerolf@gmail.com>
2025-03-12 08:32:23 +0100

    fix adapter init

--- app/misc/adapter.ts
+++ app/misc/adapter.ts
@@ -1,17 +1,18 @@
-import { NightlyConnectSuiAdapter } from '@nightlylabs/wallet-selector-sui'
+import { NightlyConnectSuiAdapter } from "@nightlylabs/wallet-selector-sui";
 
-let _adapter: NightlyConnectSuiAdapter | undefined
+let _adapter: NightlyConnectSuiAdapter | undefined;
 export const getAdapter = async (persisted = true) => {
-  if (_adapter) return _adapter
+  if (_adapter) return _adapter;
   _adapter = await NightlyConnectSuiAdapter.build(
     {
       appMetadata: {
-        name: 'Sui Template',
-        description: 'Sui Template',
-        icon: 'https://docs.nightly.app/img/logo.png',
+        name: "Sui Template",
+        description: "Sui Template",
+        icon: "https://docs.nightly.app/img/logo.png",
       },
+      persistent: persisted,
     },
-    persisted
-  )
-  return _adapter
-}
+    {}
+  );
+  return _adapter;
+};
